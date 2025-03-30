44c708103ff6f3d558392ba08cd79d4730f07735
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-03-30 13:11:07 +0200

    Added getstate function to get the current state

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function createStore(reducer) {
   let state = {count: 0}; // Initial state
     let listeners = [];
 
+    /**
+     * 
+     * @returns {Object} the current state of the store
+     */
+
+    function getState() {
+        return state;
+    }
+
+    /**
+     * 
+     * @param {Object} action - the action object containing the type of state change
+     */
+
